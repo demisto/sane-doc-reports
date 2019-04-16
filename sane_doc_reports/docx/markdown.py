9bd7979aa0e59f0d8a4ddb858b0e96f953355938
Agam More <agam.more@demisto.com>
2019-04-16 18:38:11 +0300

    Add basic markdown and text docx elements

--- sane_doc_reports/docx/markdown.py
+++ sane_doc_reports/docx/markdown.py
@@ -1,8 +1,87 @@
 from typing import Dict
 
-from sane_doc_reports.conf import DEBUG
+import mistune
+
+from sane_doc_reports.conf import DEBUG, DOMAIN, DATA_KEY, LAYOUT_KEY, STYLE_KEY
+from sane_doc_reports.docx import text
+
+
+class DocxRenderer(mistune.Renderer):
+    """
+    We need a renderer class like this so mistune will parse the markdown for
+    us and we could change the output.
+    Here we generate JSON (like sane's JSON) that we will later send to the
+    other docx elements. (Yeah it's kind of a hack of the renderer)
+    """
+    cell_object = None
+
+    def attach_sane_constructor(self):
+        # A small hack to inject the cell reference so it can be used here.
+        self.sane = []
+
+    def double_emphasis(self, text_value):
+        return f"__bold__{text_value}"
+
+    def header(self, text_value, level, raw=None):
+        bold = False
+        if '__bold__' in text_value:
+            bold = True
+            text_value = text_value.replace('__bold__', '')
+
+        font_size = 30 - level * 2
+        section = {
+            f'{DATA_KEY}': {
+                'text': text_value
+            },
+            f'{LAYOUT_KEY}': {
+                f'{STYLE_KEY}': {
+                    'name': 'Arial',
+                    'fontSize': font_size,
+                    'color': 'black',
+                    'textAlign': 'left',
+                    'bold': bold
+                }
+            }
+        }
+        self.sane.append(section)
+        return ''
+
+    def paragraph(self, text_value):
+        section = {
+            f'{DATA_KEY}': {
+                'text': text_value
+            },
+            f'{LAYOUT_KEY}': {
+                f'{STYLE_KEY}': {
+                    'name': 'Arial',
+                    'fontSize': 14,
+                    'color': '#6c6c6c',
+                    'textAlign': 'left'
+                }
+            }
+        }
+        self.sane.append(section)
+        return ''
+
+
+def insert_from_markdowm(cell_object: dict, markdown_string: str) -> None:
+    renderer = DocxRenderer()
+    renderer.attach_sane_constructor()
+
+    markdown = mistune.Markdown(renderer=renderer)
+    markdown(markdown_string)
+
+    is_first = True
+    # We need to reverse the sane list because adding paragraphs is adding them
+    # before and not after.
+    for elem in markdown.renderer.sane[::-1]:
+        text.insert(cell_object, elem, is_first)
+        is_first = False
 
 
 def insert(cell_object: Dict, section: Dict) -> None:
     if DEBUG:
-        print("Yo Im markdown chart")
+        print("Yo Im markdown")
+
+    markdown_string = section[DATA_KEY]['text']
+    insert_from_markdowm(cell_object, markdown_string)
--- sane_doc_reports/docx/text.py
+++ sane_doc_reports/docx/text.py
@@ -1,8 +1,50 @@
 from typing import Dict
 
-from sane_doc_reports.conf import DEBUG
+from sane_doc_reports.conf import LAYOUT_KEY, STYLE_KEY, DATA_KEY
+from sane_doc_reports.utils import hex_to_rgb
+from docx.shared import Pt
+import webcolors
 
 
-def insert(cell_object: Dict, section: Dict) -> None:
-    if DEBUG:
-        print("Yo Im text")
+def insert(cell_object: Dict, section: Dict, is_addition=False) -> None:
+    cell = cell_object['run']
+    paragraph = cell_object['paragraph']
+
+    if is_addition:
+        cell = cell_object['cell'].add_paragraph().add_run()
+        cell.text = section[DATA_KEY]['text']
+    else:
+        cell.text = section[DATA_KEY]['text']
+
+    if STYLE_KEY not in section[LAYOUT_KEY]:
+        return
+
+    style = section[LAYOUT_KEY][STYLE_KEY]
+    font = cell.font
+
+    # Font size
+    if 'fontSize' in style:
+        font.size = Pt(style['fontSize'])
+
+    # Font family
+    if 'name' in style:
+        font.name = style['name']
+
+    # Other characteristics
+    if 'bold' in style:
+        font.bold = style['bold']
+
+    # Font color
+    if 'color' in style:
+        if style['color'][0] != '#':
+            font.color.rgb = hex_to_rgb(webcolors.name_to_hex(style['color']))
+        else:
+            font.color.rgb = hex_to_rgb(style['color'])
+
+    # text align
+    if style['textAlign'] == 'left':
+        cell.alignment = 0
+        paragraph.paragraph_format.alignment = 0
+    elif style['textAlign'] == 'right':
+        cell.alignment = 1
+        paragraph.paragraph_format.alignment = 2

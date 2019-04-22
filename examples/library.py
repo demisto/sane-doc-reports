fae7d7ac37a8ec40be08ff7a2c6f6fc80c7ede56
Agam <agam.more@demisto.com>
2019-04-22 14:58:42 +0300

    Separate the docx tests, add the table implementation (#4)
    * Separate the docx tests, add the table implementation
    
    * Fix the table columns and add a more in-depth test
    
    * Remove the extra formmating we implemetnted

--- examples/library.py
+++ examples/library.py
@@ -4,7 +4,7 @@ from tests.utils import get_mock
 
 def main():
     # Gets the json form tests/mock_data
-    report = Report(get_mock('grid_checks/fullgrid.json'))
+    report = Report(get_mock('docx/table.json'))
     report.populate_report()
     report.save('example.docx')
 
--- sane_doc_reports/docx/table.py
+++ sane_doc_reports/docx/table.py
@@ -1,9 +1,28 @@
 from typing import Dict
 
-from sane_doc_reports.conf import DEBUG
+from sane_doc_reports.conf import DEBUG, LAYOUT_KEY
 
 
 def insert(cell_object: Dict, section: Dict) -> None:
     if DEBUG:
-        print("Yo Im table")
-    # cell.add_table(rows, cols)
+        print('Yo I am a table')
+
+    table_data = section['data']
+
+    if 'readableHeaders' in section[LAYOUT_KEY]:
+        table_columns = list(section[LAYOUT_KEY]['readableHeaders'].values())
+    else:
+        table_columns = section[LAYOUT_KEY]['tableColumns']
+
+    table = cell_object['cell'].add_table(rows=1, cols=len(table_columns))
+    table.style = 'Light Shading'
+    hdr_cells = table.rows[0].cells
+
+    for i, header in enumerate(table_columns):
+        hdr_cells[i].text = header
+
+    for r in table_data:
+        row_cells = table.add_row().cells
+        for i, header in enumerate(table_columns):
+            if header in r:
+                row_cells[i].text = r[header]

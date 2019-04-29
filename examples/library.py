8fe607e4f5248cd03b2fad123c71f0086f46e48e
Agam <agam.more@demisto.com>
2019-04-29 18:03:41 +0300

    Add docx text (#9)
    * Abstract the import and invokation of the insert function
    
    * Add text and tests for it
    
    * Add underline, italic
    
    * Remove unnecessary line
    
    * Make the examples file more sustainable
    
    * Make the styling generic to all elements

--- examples/library.py
+++ examples/library.py
@@ -2,11 +2,33 @@ from sane_doc_reports.report import Report
 from tests.utils import get_mock
 
 
-def main():
-    # Gets the json form tests/mock_data
+def example_basic():
+    report = Report(get_mock('grid_checks/fullgrid.json'))
+    report.populate_report()
+    report.save('example.docx')
+
+
+def example_table():
+    report = Report(get_mock('docx/table.json'))
+    report.populate_report()
+    report.save('example.docx')
+
+
+def example_number():
     report = Report(get_mock('docx/number_and_trend.json'))
     report.populate_report()
     report.save('example.docx')
+
+
+def example_text():
+    report = Report(get_mock('docx/text.json'))
+    report.populate_report()
+    report.save('example.docx')
+
+
+def main():
+    # Gets the json form tests/mock_data
+    example_text()
 
 
 if __name__ == '__main__':

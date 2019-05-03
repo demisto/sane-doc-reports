2a3a3394ded86fce998e5ee649ab977935c3fbef
Agam <agam.more@demisto.com>
2019-05-03 19:47:12 +0200

    Add docx pie chart (#10)
    * Add a basic pie chart
    
    * Basic usage of pie chart (donut)
    
    * Fix the utils merge
    
    * Add basic pie chart with colors and sizing
    
    * Add the pie chart's test
    
    * Fix the unassigned + it's color
    
    * Fix the deafult color if there is more than 1 unassigned
    
    * Add consts to sizing
    
    * Remove the chart numbers and add to legends

--- sane_doc_reports/utils.py
+++ sane_doc_reports/utils.py
@@ -1,11 +1,17 @@
 import base64
 import re
+import tempfile
 from io import BytesIO
 import importlib
+from pathlib import Path
 
+import matplotlib
 from docx.shared import RGBColor
 
+from matplotlib import pyplot as plt
 from matplotlib import colors as mcolors
+
+from sane_doc_reports.conf import LAYOUT_KEY, SIZE_H_INCHES, SIZE_W_INCHES, DPI
 
 colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
 
@@ -44,3 +50,45 @@ def insert_by_type(type: str, cell_object: dict, section: dict):
     """ Call a docx elemnt's insert method """
     func = importlib.import_module(f'sane_doc_reports.docx.{type}')
     func.insert(cell_object, section)
+
+
+def plot(func):
+    """ A decorator used to clear and resize each chart """
+
+    def wrapper(*args, **kwargs):
+        plt.clf()
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+def plt_t0_b64(plt: matplotlib.pyplot):
+    """ Matplotlib to base64 url """
+    path = Path(tempfile.mkdtemp()) / Path(
+        next(tempfile._get_candidate_names()) + '.png')
+
+    plt.savefig(str(path), format='png', bbox_inches='tight', figsize=(1, 1),
+                dpi=80)
+
+    with open(str(path), "rb") as f:
+        img_base64 = base64.b64encode(f.read()).decode("utf-8", "ignore")
+        b64 = f'data:image/png;base64,{img_base64}'
+
+    path.unlink()
+    return b64
+
+
+def convert_plt_size(section):
+    """ Convert the plot size from pixels to word """
+    size_w, size_h, dpi = (SIZE_W_INCHES, SIZE_H_INCHES, DPI)
+    if 'dimensions' in section[LAYOUT_KEY]:
+        h = section[LAYOUT_KEY]['dimensions']['height'] / 100.0
+        w = section[LAYOUT_KEY]['dimensions']['width'] / 100.0
+        size_w, size_h, dpi = (w, h, 100)
+    return size_w, size_h, dpi
+
+
+def get_saturated_colors():
+    """ Return named colors that are clearly visible on a white background """
+    return [name for name, _ in colors.items()
+            if 'light' not in name and 'white' not in name]

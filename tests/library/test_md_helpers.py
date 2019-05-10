bbc54fb71fc019d96ead7d81b4d09a29bea0bada
Agam More <agam.more@demisto.com>
2019-05-10 19:37:37 +0300

    Add more tests to fix wrapp text

--- tests/library/test_md_helpers.py
+++ tests/library/test_md_helpers.py
@@ -1,6 +1,4 @@
-
 from pyquery import PyQuery as pq, PyQuery
-
 
 from sane_doc_reports.MarkdownSection import markdown_to_html
 from sane_doc_reports.md_helpers import fix_unwrapped_text
@@ -89,12 +87,59 @@ def test_markdown_to_html_text_styles():
     assert markdown_to_html(md_input) == ex_output
 
 
+def test_fix_unwrapped_no_tags():
+    html = 'test'
+    root_elem = pq(html)
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<p>test</p>')
+    assert res_check == expected.outer_html()
+
+
+def test_fix_unwrapped_em_tag():
+    html = '<em>wrapped</em>'
+    root_elem = pq(html)
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<em>wrapped</em>')
+    assert res_check == expected.outer_html()
+
+
 def test_fix_unwrapped_text_basic():
     html = '<p>1<b>2</b>3</p>'
     root_elem = pq(html)
     res = fix_unwrapped_text(root_elem)
     res_check = res.outer_html()
     expected = pq('<p><span>1</span><span><b>2</b></span><span>3</span></p>')
+    assert res_check == expected.outer_html()
+
+
+def test_fix_unwrapped_text_basic_2():
+    html = '<p><b>2</b>3</p>'
+    root_elem = pq(html)
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<p><span><b>2</b></span><span>3</span></p>')
+    assert res_check == expected.outer_html()
+
+
+def test_fix_unwrapped_text_basic_3():
+    html = '<p><strong>test</strong> unwrapped</p>'
+    root_elem = pq(html)
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<p><span><strong>test</strong></span><span> unwrapped' +
+                  '</span<</p>')
+    assert res_check == expected.outer_html()
+
+
+def test_fix_unwrapped_text_basic_4():
+    html = '<p><i>a</i><b>b</b><c>c</c></p>'
+    root_elem = pq(html)
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<p><span><i>a</i></span><span><b>b</b></span><span><c>c' +
+                  '</c></span></p>')
     assert res_check == expected.outer_html()
 
 
@@ -109,13 +154,23 @@ def test_fix_unwrapped_text_deep():
     assert res_check == expected.html()
 
 
-def test_fix_unwrapped_text_unwrapped():
-    html = '<p><i>a</i><b>b</b><c>c</c></p>'
+def test_fix_unwrapped_text_attributes():
+    html = '<p><strong attr="123">test</strong> unwrapped</p>'
+    root_elem = pq(html)
+
+    res = fix_unwrapped_text(root_elem)
+    res_check = res.outer_html()
+    expected = pq('<p><span><strong attr="123">test</strong></span>' +
+                  '<span> unwrapped</span></p>')
+    assert res_check == expected.outer_html()
+
+
+def test_fix_unwrapped_no_unwrapped_basic():
+    html = '<span>wrapped</span>'
     root_elem = pq(html)
     res = fix_unwrapped_text(root_elem)
     res_check = res.outer_html()
-    expected = pq('<p><span><i>a</i></span><span><b>b</b></span><span><c>c' +
-                  '</c></span></p>')
+    expected = pq('<span>wrapped</span>')
     assert res_check == expected.outer_html()
 
 
@@ -128,7 +183,7 @@ def test_fix_unwrapped_text_no_unwrapped_basic():
     assert res_check == expected.html()
 
 
-def test_fix_unwrapped_text_no_unwrapped_basic2():
+def test_fix_unwrapped_text_no_unwrapped_basic_2():
     html = '<p><span>123</span></p>'
     root_elem = pq(html)
     res = fix_unwrapped_text(root_elem)
@@ -146,16 +201,7 @@ def test_fix_unwrapped_text_no_unwrapped_complex():
     assert res_check == expected.outer_html()
 
 
-def test_fix_unwrapped_text_basic2():
-    html = '<p><b>2</b>3</p>'
-    root_elem = pq(html)
-    res = fix_unwrapped_text(root_elem)
-    res_check = res.outer_html()
-    expected = pq('<p><span><b>2</b></span><span>3</span></p>')
-    assert res_check == expected.outer_html()
-
-
-def test_fix_unwrapped_text_basic_3():
+def test_fix_unwrapped_text_ul_basic():
     html = '<ul><li>123</li></ul>'
     root_elem = pq(html)
     res = fix_unwrapped_text(root_elem)
@@ -187,8 +233,9 @@ def test_fix_unwrapped_text_complex():
     assert res_check == expected.outer_html()
 
 
-def test_fix_unwrapped_text_complex2():
-    html = '<p>aaa <em>bbb <i>ccc<q>zzz</q>ddd</i></em> ddd <del>eee</del> fff</p>'
+def test_fix_unwrapped_text_complex_2():
+    html = '<p>aaa <em>bbb <i>ccc<q>zzz</q>ddd</i></em> ddd <del>' + \
+           'eee</del> fff</p>'
     root_elem = pq(html)
     res = fix_unwrapped_text(root_elem)
     res_check = res.outer_html()
@@ -199,7 +246,7 @@ def test_fix_unwrapped_text_complex2():
     assert res_check == expected.outer_html()
 
 
-def test_fix_unwrapped_text_complex3():
+def test_fix_unwrapped_text_complex_3():
     html = '<p>aaa <em>bbb <i>ccc<span><p>zzz</p></span>ddd</i>' + \
            '</em> ddd <del>eee</del> fff</p>'
     root_elem = pq(html)
@@ -225,90 +272,3 @@ def test_no_change_fix_unwrapped_text_complex():
                   '</span></i></span></em></span><span> ddd </span><span>' +
                   '<del>eee</del></span><span> fff</span></p>')
     assert res_check == expected.outer_html()
-# def test_fix_unwrapped_text_stop_condition_unchanged():
-#     html_input = 'unwrapped'
-#     ex_output = 'unwrapped'
-#     assert fix_unwrapped_text(html_input, already_wrapped=True) == ex_output
-#
-#
-# def test_fix_unwrapped_text_stop_condition():
-#     html_input = '<em>unwrapped</em>'
-#     ex_output = '<em>unwrapped</em>'
-#     assert fix_unwrapped_text(html_input, already_wrapped=True) == ex_output
-
-
-# def test_fix_unwrapped_text_basic():
-#     html_input = 'wrapped'
-#     ex_output = '<span>wrapped</span>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_unchanged_basic():
-#     html_input = '<span>wrapped</span>'
-#     ex_output = '<span>wrapped</span>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_basic_p():
-#     html_input = '<p><strong>test</strong> unwrapped</p>'
-#     ex_output = '<p><span><strong>test</strong></span><span> unwrapped' + \
-#                 '</span></p>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_basic_attrib():
-#     html_input = '<p><strong attr="123">test</strong> unwrapped</p>'
-#     ex_output = '<p><span><strong attr="123">test</strong></span><span>' + \
-#                 ' unwrapped</span></p>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_deep():
-#     html_input = '<p><strong><em>test</em> unwrapped</strong></p>'
-#     ex_output = '<p><span><strong><span><em>test</em></span><span>' + \
-#                 ' unwrapped</span></strong></span></p>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_unchanged_deep():
-#     html_input = '<span><span>wrapped</span></span>'
-#     ex_output = '<span><span>wrapped</span></span>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_unchanged_deep_other_tags():  # MAY allow inner span
-#     html_input = '<span><em>wrapped</em></span>'
-#     ex_output = '<span><em>wrapped</em></span>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_complex_deep():
-#     html_input = '<p>aaa <em>bbb <i>ccc<q>zzz</q>ddd</i></em> ddd <del>' + \
-#                  'eee</del> fff</p>'
-#     ex_output = '<p><span>aaa </span><span><em><span>bbb </span>' + \
-#                 '<span><i><span>ccc</span><span><q><span>zzz</span></q>' + \
-#                 '</span><span>ddd</span></i></span></em></span><span> ddd ' + \
-#                 '</span><span><del>eee</del></span><span> fff</span></p>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# def test_fix_unwrapped_text_complex_deep_elements():
-#     html_input = '<p>aaa <em>bbb <i>ccc<span><p>zzz</p></span>ddd</i>' + \
-#                  '</em> ddd <del>eee</del> fff</p>'
-#     ex_output = '<p><span>aaa </span><span><em><span>bbb </span>' + \
-#                 '<span><i><span>ccc</span><span><p>zzz</p>' + \
-#                 '</span><span>ddd</span></i></span></em></span><span> ddd ' + \
-#                 '</span><span><del>eee</del></span><span> fff</span></p>'
-#     assert fix_unwrapped_text(html_input) == ex_output
-#
-#
-# # def test_fix_unwrapped_text_unchanged_complex():
-# #     html_input = '<p><span>aaa </span><span><em><span>bbb </span>' + \
-# #                 '<span><i><span>ccc</span><span><p>zzz</p>' + \
-# #                 '</span><span>ddd</span></i></span></em></span><span> ddd ' + \
-# #                 '</span><span><del>eee</del></span><span> fff</span></p>'
-# #     ex_output = '<p><span>aaa </span><span><em><span>bbb </span>' + \
-# #                 '<span><i><span>ccc</span><span><p>zzz</p>' + \
-# #                 '</span><span>ddd</span></i></span></em></span><span> ddd ' + \
-# #                 '</span><span><del>eee</del></span><span> fff</span></p>'
-# #     assert fix_unwrapped_text(html_input) == ex_output

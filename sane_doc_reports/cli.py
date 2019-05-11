a95e9951a105fac4eee11566a8a8b6142adfacc0
Agam More <agam.more@demisto.com>
2019-05-11 15:05:30 +0300

    Add cli tests

--- sane_doc_reports/cli.py
+++ sane_doc_reports/cli.py
@@ -1,10 +1,17 @@
 import click
 
+from sane_doc_reports.Report import Report
+
 
 @click.command()
-def main():
-    # TODO: Need to implement this
-    print("Called main")
+@click.argument('input', type=click.Path(exists=True, readable=True))
+@click.argument('output', type=click.Path())
+def main(input, output):
+    report = Report(input)
+    report.populate_report()
+    report.save(output)
+
+    click.echo('Converted successfully.')
 
 
 if __name__ == "__main__":
--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,10 +1,34 @@
+from pathlib import Path
+
 from sane_doc_reports.cli import main
 from click.testing import CliRunner
 
 
-def test_help():
+def test_cli_help():
     runner = CliRunner()
     result = runner.invoke(main, ['--help'])
     assert result.exit_code == 0
     assert 'Show this message and exit' in result.output
 
+
+def test_cli_bad_input():
+    runner = CliRunner()
+    result = runner.invoke(main, ['derp'])
+    assert result.exit_code == 2
+    result = runner.invoke(main, ['', 'merp'])
+    assert result.exit_code == 2
+    result = runner.invoke(main, ['derp', 'merp'])
+    assert result.exit_code == 2
+
+
+def test_cli_creating_file():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('test.json', 'w') as f:
+            f.write('''[{ "type": "text", "data": { "text": "Test"},
+                "layout": { "columnPos": 0, "h": 1, "rowPos": 0, "w": 12 }}]''')
+
+        result = runner.invoke(main, ['test.json', 'out.docx'])
+        assert result.exit_code == 0
+        assert result.output == 'Converted successfully.\n'
+        assert Path('out.docx').is_file()

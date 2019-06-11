98a078226b8338f1db603a865c72f55bb172bd02
Agam More <agam.more@demisto.com>
2019-06-11 17:17:11 +0300

    Rename empty to unimplemented

--- sane_doc_reports/elements/unimplemented.py
+++ sane_doc_reports/elements/unimplemented.py
@@ -3,12 +3,12 @@ from sane_doc_reports.conf import DEBUG
 from sane_doc_reports.elements import error, md_hr
 
 
-class EmptyElement(Element):
+class UnimplementedElement(Element):
     """ Used to continue generator docx even with unsupported types """
 
     def insert(self):
         if DEBUG:
-            print('Adding divider...')
+            print('Adding unimplemented...')
 
         self.section.contents = f'{self.section.type} is not implemented' + \
                                 ' in Docx yet. If required, use PDF instead.'
@@ -16,4 +16,4 @@ class EmptyElement(Element):
 
 
 def invoke(cell_object, section) -> None:
-    EmptyElement(cell_object, section).insert()
+    UnimplementedElement(cell_object, section).insert()

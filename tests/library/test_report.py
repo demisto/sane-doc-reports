d7a73554e469262086a4fe9735c192b17b9245e4
Agam More <agam.more@demisto.com>
2019-08-08 17:47:32 +0300

    Add paper size

--- tests/library/test_report.py
+++ tests/library/test_report.py
@@ -4,6 +4,8 @@ from sane_doc_reports.populate.grid import get_vtable_merged
 from sane_doc_reports.populate.Report import Report
 from tests import utils
 from tests.utils import _transform
+from sane_doc_reports.conf import A4_MM_WIDTH, A4_MM_HEIGHT, A3_MM_WIDTH, \
+    A3_MM_HEIGHT, LETTER_MM_HEIGHT, LETTER_MM_WIDTH
 
 
 def test_creation_of_report_layout_basic():
@@ -47,7 +49,127 @@ def test_creation_of_report_layout_full():
     assert get_vtable_merged(table) == vtable
 
     # Check the page breaks
-    assert len(d.element.xpath('//w:br')) == 0
+    assert len(d.element.xpath('//w:pgSz[@w:orient="landscape"]')) == 0
+
+    # Check page size
+    page_sz = d.element.xpath('//w:pgSz[@w:w]')
+    assert len(page_sz) == 1
+    page_sz = page_sz[0]
+    dw, dh = int(page_sz.w.mm), int(page_sz.h.mm)
+    w, h = A4_MM_WIDTH.mm, A4_MM_HEIGHT.mm
+    assert abs(w - dw) < 2
+    assert abs(h - dh) < 2
+
+
+def test_creation_of_report_layout_full_a3():
+    report = Report(*_transform('grid_checks/fullgrid.json'), options={
+        'paper_size': 'A3'
+    })
+    report.populate_report()
+
+    d = report.document
+    table = next(utils.iter_block_items(d))
+    assert isinstance(table, Table)
+    assert len(table.columns) == 12
+    assert len(table.rows) == 12
+
+    # Check the specific merged cells
+    vtable = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    assert get_vtable_merged(table) == vtable
+
+    # Check the page breaks
+    assert len(d.element.xpath('//w:pgSz[@w:orient="landscape"]')) == 0
+
+    # Check page size
+    page_sz = d.element.xpath('//w:pgSz[@w:w]')
+    assert len(page_sz) == 1
+    page_sz = page_sz[0]
+    dw, dh = int(page_sz.w.mm), int(page_sz.h.mm)
+    w, h = A3_MM_WIDTH.mm, A3_MM_HEIGHT.mm
+    assert abs(w - dw) < 2
+    assert abs(h - dh) < 2
+
+
+def test_creation_of_report_layout_full_letter():
+    report = Report(*_transform('grid_checks/fullgrid.json'), options={
+        'paper_size': 'letter'
+    })
+    report.populate_report()
+
+    d = report.document
+    table = next(utils.iter_block_items(d))
+    assert isinstance(table, Table)
+    assert len(table.columns) == 12
+    assert len(table.rows) == 12
+
+    # Check the specific merged cells
+    vtable = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    assert get_vtable_merged(table) == vtable
+
+    # Check the page breaks
+    assert len(d.element.xpath('//w:pgSz[@w:orient="landscape"]')) == 0
+
+    # Check page size
+    page_sz = d.element.xpath('//w:pgSz[@w:w]')
+    assert len(page_sz) == 1
+    page_sz = page_sz[0]
+    dw, dh = int(page_sz.w.mm), int(page_sz.h.mm)
+    w, h = LETTER_MM_WIDTH.mm, LETTER_MM_HEIGHT.mm
+    assert abs(w - dw) < 2
+    assert abs(h - dh) < 2
+
+
+def test_creation_of_report_layout_full_landscape():
+    report = Report(*_transform('grid_checks/fullgrid.json'), options={
+        'orientation': 'landscape'
+    })
+    report.populate_report()
+
+    d = report.document
+    table = next(utils.iter_block_items(d))
+    assert isinstance(table, Table)
+    assert len(table.columns) == 12
+    assert len(table.rows) == 12
+
+    # Check the specific merged cells
+    vtable = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    assert get_vtable_merged(table) == vtable
+
+    # Check the page breaks
+    assert len(d.element.xpath('//w:pgSz[@w:orient="landscape"]')) == 1
+
+    # Check page size
+    page_sz = d.element.xpath('//w:pgSz[@w:w]')
+    assert len(page_sz) == 1
+    page_sz = page_sz[0]
+    dw, dh = int(page_sz.w.mm), int(page_sz.h.mm)
+    h, w = A4_MM_WIDTH.mm, A4_MM_HEIGHT.mm  # orientation
+    assert abs(w - dw) < 2
+    assert abs(h - dh) < 2
 
 
 def test_creation_of_report_layout_full_paged():
@@ -74,6 +196,7 @@ def test_creation_of_report_layout_full_paged():
 
     # Check the page breaks
     assert len(d.element.xpath('//w:br')) == 1
+
 
 def test_creation_of_report_layout_merged():
     report = Report(*_transform('grid_checks/mergegrid.json'))

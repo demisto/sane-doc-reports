fc4487ebe90ad82b6850108669dd72daba419bfa
Agam More <agam.more@demisto.com>
2019-10-02 16:09:40 +0300

    Add 63 cols test

--- tests/library/elements/test_table.py
+++ tests/library/elements/test_table.py
@@ -18,7 +18,7 @@ def test_table_in_report():
     assert len(d.element.xpath('//w:tbl//w:tbl')) == 1
 
     # Check that it has the right amount of rows
-    assert len(d.element.xpath('//w:tbl//w:tbl//w:t')) == 22
+    assert len(d.element.xpath('//w:tbl//w:tbl//w:t')) == 23 # Avatar isn't hidden
 
 
 def test_table_in_report_widget():
@@ -44,7 +44,8 @@ def test_table_63_cols():
     table = next(utils.iter_block_items(d))
     assert isinstance(table, Table)
     assert len(table.columns) == 12
-    assert len(table.rows) == 3
+    assert len(table.rows) == 2
 
     # Check we have the max amount of table columns.
     assert len(d.element.xpath('//w:tbl//w:tbl')) == 1
+    assert int(d.element.xpath('count(//w:t)')) == 64 # 63 + title

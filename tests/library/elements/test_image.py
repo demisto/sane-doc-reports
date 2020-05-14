3b6bc2aec6b58a5dfe2fae977d8913fbefad4481
Agam More <agmore@paloaltonetworks.com>
2020-05-14 16:03:36 +0300

    Add check for svgexport

--- tests/library/elements/test_image.py
+++ tests/library/elements/test_image.py
@@ -1,3 +1,5 @@
+from shutil import which
+
 from docx.table import Table
 
 from sane_doc_reports.conf import SHOULD_HAVE_12_GRID
@@ -23,11 +25,14 @@ def test_picture_in_report():
     # Check that there is indeed an image
     assert len(d.element.xpath('//pic:pic')) == 1
 
+
 def test_svg_image():
+    assert which('svgexport') is not None
     report = Report(*_transform('elements/image-svg.json'))
     report.populate_report()
     d = report.document
     assert len(d.element.xpath('//pic:pic')) == 1
+
 
 def test_remote_image():
     report = Report(*_transform('elements/image-remote.json'))

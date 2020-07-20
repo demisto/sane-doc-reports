b7f4983d315d7582b9a9ea39f9fcfafd72f5fb72
Agam More <agmore@paloaltonetworks.com>
2020-07-20 17:46:46 +0300

    Fix the trend values and arrow signs

--- tests/library/elements/test_number_and_trend.py
+++ tests/library/elements/test_number_and_trend.py
@@ -21,15 +21,19 @@ def test_number_and_trend_in_report():
         assert len(table.rows) == 4
 
     # Check that there is indeed a table within a table
-    assert len(d.element.xpath('//w:tbl//w:tbl')) == 3
+    assert len(d.element.xpath('//w:tbl//w:tbl')) == 4
 
     # Check that it has the right amount of rows
-    assert len(d.element.xpath('//w:tbl//w:tbl//w:t')) == 8
+    assert len(d.element.xpath('//w:tbl//w:tbl//w:t')) == 11
 
     # Check that there is an extra sign
     assert len(
-        d.element.xpath("//w:tbl//w:tbl//w:t[contains(text(),'%')]")) == 3
+        d.element.xpath("//w:tbl//w:tbl//w:t[contains(text(),'%')]")) == 4
 
-    # Check that precentage is correct
+    # Check that percentage is correct
     assert len(
         d.element.xpath("//w:tbl//w:tbl//w:t[contains(text(),'100.25%')]")) == 1
+
+    # Check that percentage is correct
+    assert len(
+        d.element.xpath("//w:tbl//w:tbl//w:t[contains(text(),'>999%')]")) == 1

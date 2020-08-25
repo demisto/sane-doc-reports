48e29dfdacafbcf76104b4de1c58320f8638118c
Hagai Revah <hrevah@paloaltonetowrks.com>
2020-08-25 18:05:38 +0300

    fix readable headers

--- examples/library.py
+++ examples/library.py
@@ -3,124 +3,124 @@ from sane_doc_reports.conf import XSOAR_LOGO_BASE64
 from tests.utils import get_mock
 
 
-def example_basic(out_file_name='example.docx'):
+def example_basic(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/fullgrid.json', ret_dict=False),
              out_file_name)
 
 
-def example_paged(out_file_name='example.docx'):
+def example_paged(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/fullgridpaged.json', ret_dict=False),
              out_file_name)
 
 
-def example_paged_complex(out_file_name='example.docx'):
+def example_paged_complex(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/complexpaged.json', ret_dict=False),
              out_file_name)
 
 
-def example_table(out_file_name='example.docx'):
+def example_table(out_file_name='example.zip'):
     main.run(get_mock('elements/table.json', ret_dict=False), out_file_name)
 
 
-def example_table_new(out_file_name='example.docx'):
+def example_table_new(out_file_name='example.zip'):
     main.run(get_mock('elements/table_new_json.json', ret_dict=False),
              out_file_name)
 
 
-def example_number_and_trend(out_file_name='example.docx'):
+def example_number_and_trend(out_file_name='example.zip'):
     main.run(get_mock('elements/number_and_trend.json', ret_dict=False),
              out_file_name)
 
 
-def example_text(out_file_name='example.docx'):
+def example_text(out_file_name='example.zip'):
     main.run(get_mock('elements/text.json', ret_dict=False), out_file_name)
 
 
-def example_pie_chart(out_file_name='example.docx'):
+def example_pie_chart(out_file_name='example.zip'):
     main.run(get_mock('elements/pie_chart.json', ret_dict=False),
              out_file_name)
 
 
-def example_markdown(out_file_name='example.docx'):
+def example_markdown(out_file_name='example.zip'):
     main.run(get_mock('elements/markdown.json', ret_dict=False), out_file_name)
 
 
-def example_markdown_md_button(out_file_name='example.docx'):
+def example_markdown_md_button(out_file_name='example.zip'):
     main.run(get_mock('elements/markdown_md_button.json', ret_dict=False),
              out_file_name)
 
 
-def example_markdown_paged(out_file_name='example.docx'):
+def example_markdown_paged(out_file_name='example.zip'):
     main.run(get_mock('elements/markdown_paged.json', ret_dict=False),
              out_file_name)
 
 
-def example_hr(out_file_name='example.docx'):
+def example_hr(out_file_name='example.zip'):
     main.run(get_mock('elements/hr.json', ret_dict=False), out_file_name)
 
 
-def example_line_chart(out_file_name='example.docx'):
+def example_line_chart(out_file_name='example.zip'):
     main.run(get_mock('elements/line_chart.json', ret_dict=False),
              out_file_name)
 
 
-def example_old_json(out_file_name='example.docx'):
+def example_old_json(out_file_name='example.zip'):
     main.run(get_mock('old_json.json', ret_dict=False),
              out_file_name)
 
 
-def _example_junk(out_file_name='example.docx'):
+def _example_junk(out_file_name='example.zip'):
     # Generate a big elements file for testing
     main.run(get_mock('junkbig.json', ret_dict=False), out_file_name)
 
 
-def example_bar_chart(out_file_name='example.docx'):
+def example_bar_chart(out_file_name='example.zip'):
     main.run(get_mock('elements/bar_chart.json', ret_dict=False),
              out_file_name)
 
 
-def example_duration(out_file_name='example.docx'):
+def example_duration(out_file_name='example.zip'):
     main.run(get_mock('elements/duration.json', ret_dict=False), out_file_name)
 
 
-def example_unimplemented(out_file_name='example.docx'):
+def example_unimplemented(out_file_name='example.zip'):
     main.run(get_mock('elements/unimplemented.json', ret_dict=False),
              out_file_name)
 
 
-def example_orientation_landscape(out_file_name='example.docx'):
+def example_orientation_landscape(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/fullgrid.json', ret_dict=False),
              out_file_name, {'orientation': 'landscape'})
 
 
-def example_paper_size_a3(out_file_name='example.docx'):
+def example_paper_size_a3(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/fullgrid.json', ret_dict=False),
              out_file_name, {'paper_size': 'A3'})
 
 
-def example_list(out_file_name='example.docx'):
+def example_list(out_file_name='example.zip'):
     main.run(get_mock('elements/list.json', ret_dict=False), out_file_name)
 
 
-def example_date(out_file_name='example.docx'):
+def example_date(out_file_name='example.zip'):
     main.run(get_mock('elements/date.json', ret_dict=False), out_file_name)
 
 
-def example_items_section(out_file_name='example.docx'):
+def example_items_section(out_file_name='example.zip'):
     main.run(get_mock('elements/items_section.json', ret_dict=False),
              out_file_name)
 
 
-def example_image_remote(out_file_name='example.docx'):
+def example_image_remote(out_file_name='example.zip'):
     main.run(get_mock('elements/image-remote.json', ret_dict=False),
              out_file_name)
 
 
-def example_image_svg(out_file_name='example.docx'):
+def example_image_svg(out_file_name='example.zip'):
     main.run(get_mock('elements/image-svg.json', ret_dict=False), out_file_name)
 
 
-def example_header_logo(out_file_name='example.docx'):
+def example_header_logo(out_file_name='example.zip'):
     main.run(get_mock('grid_checks/fullgrid.json', ret_dict=False),
              out_file_name, options={
             'customerLogo': XSOAR_LOGO_BASE64,
@@ -129,7 +129,7 @@ def example_header_logo(out_file_name='example.docx'):
 
 
 def example():
-    main.run(get_mock('example.json', ret_dict=False), 'example.docx')
+    main.run(get_mock('example.json', ret_dict=False), 'example.zip')
 
 
 def example_all():

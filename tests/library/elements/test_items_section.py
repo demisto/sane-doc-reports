231a28d3295b17ba8af2af78fd990ed6c26ef134
Agam More <agmore@paloaltonetworks.com>
2021-09-13 19:00:11 +0300

    Fix the unit tests for item sections

--- tests/library/elements/test_items_section.py
+++ tests/library/elements/test_items_section.py
@@ -18,8 +18,7 @@ def test_items_section_in_report():
     assert len(d.sections[0].header._element.xpath('.//w:drawing')) == 1
 
     # Check there are enough itemsSections
-    assert len(d.element.xpath('//w:tbl//w:tbl[not(*/w:tblStyle) and .//w:sz['
-                               '@w:val="18"]]')) == 5
+    assert len(d.element.xpath('//w:tbl//w:tbl//w:sz[@w:val="28"]')) == 5
 
     # Check values
     assert len(d.element.xpath('//w:tbl//w:t[contains(text(), "Bot")]')) == 6
